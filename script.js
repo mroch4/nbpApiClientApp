af04649ab1a3907a007998488548ba74252b6b2f
Marcin Rochowski <rochowski.marcin@gmail.com>
2023-05-15 10:50:41 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+var alertTypes = ['alert-danger', 'alert-primary', 'alert-success', 'alert-warning']
+
+var cashInput = document.getElementsByTagName('input')[0]
+var currencyDropdown = document.getElementsByClassName('form-select')[0]
+var messageBox = document.getElementById('messageBox')
+
+var baseUrl = 'http://api.nbp.pl/api/exchangerates/rates/a'
+
+async function getCurrencyData(currency) {
+    const response = await fetch(`${baseUrl}/${currency}/?format=json`);
+    return await response.json()
+}
+
+async function submitHandler() {
+    var cashInputValue = cashInput.value
+    var currencyDropdownVlaue = currencyDropdown.value
+
+    if (cashInputValue && currencyDropdownVlaue) {
+        updateMessageBox(alertTypes[3], 'Trwa kalkulacja...')
+
+        var data = await getCurrencyData(currencyDropdown.value)
+        var currencyRate = data.rates[0].mid
+        var computedValue = (cashInputValue * currencyRate).toFixed(2)
+
+        updateMessageBox(alertTypes[2], `${cashInputValue} ${currencyDropdownVlaue.toUpperCase()} to ${computedValue} PLN`)
+    } else {
+        updateMessageBox(alertTypes[0], 'Brak danych niezbędnych do obliczeń.')
+    }
+}
+
+function updateMessageBox(alertType, message) {
+    messageBox.classList.remove(...alertTypes)
+    messageBox.classList.add(alertType)
+    messageBox.innerText = message
+}
